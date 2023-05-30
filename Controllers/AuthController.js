59646cc21fe5658cb76d9deaf5ce7b73680a9461
AnkitVlekhak <102821100+AnkitVlekhak@users.noreply.github.com>
2023-05-30 22:44:41 +0530

    Update AuthController.js

--- Controllers/AuthController.js
+++ Controllers/AuthController.js
@@ -17,7 +17,7 @@ export const registerUser = async (req, res) => {
     const user = await newUser.save();
     const token = jwt.sign({
       username: user.username, id: user._id
-    }, process.env.JWT_KEY, { expiresIn: "1h" })
+    }, "MyHome2022", { expiresIn: "1h" })
     res.status(200).json({ user, token });
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -42,7 +42,7 @@ export const loginUser = async (req, res) => {
       } else {
         const token = jwt.sign({
           username: user.username, id: user._id
-        }, process.env.JWT_KEY, { expiresIn: "1h" })
+        }, "MyHome2022", { expiresIn: "1h" })
         res.status(200).json({ user, token });
       }
 
